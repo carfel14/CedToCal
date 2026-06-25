504bba3d21149fa461bcb9c4848b4f6cf5e352c3
Carlos <carfel0609@gmail.com>
2026-06-24 23:17:13 -0600

    docs: linked privacy from homepage

--- config.js
+++ config.js
@@ -1,3 +1,2 @@
 // Client ID público de Google OAuth (visible en el navegador; lo protegen
-// los "Orígenes autorizados de JavaScript" en Google Cloud, no el ocultarlo).
 window.GOOGLE_CLIENT_ID = "3655632081-s0n8vlmut9ptpq7kl1mdkigoelr3vnf9.apps.googleusercontent.com";
